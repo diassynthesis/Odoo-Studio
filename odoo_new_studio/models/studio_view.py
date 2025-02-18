967218a3ca68823d737393bcc4dbf7758abd14f4
Med Said BARA <diassynthesis@gmail.com>
2025-02-18 01:58:32 +0100

    Update studio_view.py

--- odoo_new_studio/models/studio_view.py
+++ odoo_new_studio/models/studio_view.py
@@ -2,11 +2,11 @@ from odoo import models, fields
 
 class StudioView(models.Model):
     _name = 'studio.view'
-#    _inherit = 'studio.view'
+#   _inherit = 'studio.view'
     _description = 'Custom Views'
 
     name = fields.Char(string="View Name", required=True)
-    model_id = fields.Many2one('studio.model', string="Target Model", required=True)
+    model_id = fields.Many2one('ir.model', string="Target Model", required=True)
     view_type = fields.Selection([
         ('form', 'Form'),
         ('tree', 'List'),
